6273d9b2fdbc1c91c220e9c4c72ebd172a364520
Mike Almond <mike.almond@madebypi.co.uk>
2013-10-29 14:41:53 +0000

    more run_phantom.js tweaks - still have issues running tests in phantomjs... - added screengrab exports (success/failed png) to sanity-check the phantomjs output - see failed.png to see the test-frame not initialising... - added defer="defer" to the js_runner-html.mtt to try and make sure it runs once everything else is ready in phantomjs

--- src/resource/run_phantom.js
+++ src/resource/run_phantom.js
@@ -1,5 +1,17 @@
-(function(){
+(function(phantom){
 	"use strict";
+	
+	phantom.onError = function(msg, trace) {
+		var msgStack = ['phantom.onError: ' + msg];
+		if (trace && trace.length) {
+			msgStack.push('trace:');
+			trace.forEach(function(t) {
+				msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function + ')' : ''));
+			});
+		}
+		console.error(msgStack.join('\n'));
+		phantom.exit(1);
+	};
 	
 	var
 	
@@ -25,9 +37,12 @@
 	
 	checkTestsComplete = function(){
 		var data = getPageData();
+		
+		console.log("waiting... ");
+		
 		return typeof data!=='undefined' && data.count > 0 && data.count > 0 && data.count == data.total;
 	},
-		
+	
 	waitInterval = -1,
 	waitForResults = function(){
 		if(checkTestsComplete()){
@@ -45,17 +60,6 @@
 			console.log('[PhantomJS] Error: no page URL supplied...');
 			phantom.exit(1);
 		} else {
-			phantom.onError = function(msg, trace) {
-				var msgStack = ['phantom.onError: ' + msg];
-				if (trace && trace.length) {
-					msgStack.push('trace:');
-					trace.forEach(function(t) {
-						msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function + ')' : ''));
-					});
-				}
-				console.error(msgStack.join('\n'));
-				phantom.exit(1);
-			};
 		
 			// start up...
 			console.log("[PhantomJS] Loading page at:", url);
@@ -65,18 +69,19 @@
 				//console.log(page.content);
 				
 				if(state == "success"){
+					
 					if(checkTestsComplete()){
 						console.log("[PhantomJS] Tests already completed on page-load!");
 						setTimeout(function(){
 							phantom.exit();
-						}, 1000);
+						}, 5000);
 						
 					} else {
 						
 						var data = getPageData();
 						if(typeof data !== 'undefined'){
 							console.log("[PhantomJS] Running " + data.total + " test(s)...");
-							waitInterval = setInterval(waitForResults, 250);
+							waitInterval = setInterval(waitForResults, 500);
 						} else {
 							console.log("[PhantomJS] Tests data not found in loaded page - exit");
 							phantom.exit();
@@ -92,13 +97,14 @@
 	},
 
 
-	setupPageCallbacks = function(page){
+	setupPageCallbacks = function(){
 		
 		/** Handle js ballback from the clientside - window.callPhantom({ yourData:'here'}); */
 		page.onCallback = function(data) {
 			if(logEverything) console.log("page.onCallback");
 			
 			if(data.action == "shutdown"){
+				page.render("../success.png");
 				phantom.exit(0);
 			} else if(data.action == 'testComplete'){
 				console.log("test-complete (" + data.count + "/" + data.maxTests + ")");
@@ -183,11 +189,12 @@
 	
 	setTimeout(function(){
 		console.log("[PhantomJS] Took more than 30 seconds. Quitting");
+		page.render("../failed.png");
 		phantom.exit(1);
 	}, 30000);
 	
-	setupPageCallbacks(page);
+	setupPageCallbacks();
 	
 	start();
 	
-}());
+}(phantom));
